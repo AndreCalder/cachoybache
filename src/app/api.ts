d0fbb2340ebe3e5497e33cc78915c0e4a2999a14
AndreCalder <andre.calderonbarbeito@gmail.com>
2025-07-12 12:43:11 -0700

    Updates api route

--- src/app/api.ts
+++ src/app/api.ts
@@ -1,8 +1,8 @@
 import axios from "axios";
 
-// https://cachoybache-dot-mlai-434520.uc.r.appspot.com
+//
 const axiosInstance = axios.create({
-  baseURL: "http://127.0.0.1:8080",
+  baseURL: "https://cachoybache-dot-mlai-434520.uc.r.appspot.com",
   headers: {
     "Content-type": "application/json",
   },
